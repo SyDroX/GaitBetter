d826f16d0c2b28610658aa5bd2fe585cb2714a92
Kostya Sydoruk <sydoruk.kostya@gmail.com>
2022-10-03 12:53:35 +0300

    Added movement logic

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,20 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
     private Transform _transform;
-    [SerializeField] private float speed;
+    private float _minY;
     
-
-    void Awake()
+    [SerializeField] private float _forwardSpeed = 1;
+    [SerializeField] private float _footArcHeight = 1;
+    [SerializeField] private float _footArcSpeed = 1;
+    [SerializeField] private Transform _leftFoot;
+    [SerializeField] private Transform _rightFoot;
+    
+    private void Awake()
     {
         _transform = transform;
+        // Assuming both feet have the same minimum Y value. Left is taken arbitrarily
+        _minY = _leftFoot.transform.localPosition.y;
     }
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        _transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+        _transform.Translate(Vector3.forward * _forwardSpeed * Time.deltaTime, Space.Self);
+        float footArc = Mathf.Sin(_transform.position.z * _footArcSpeed);
+        SetFootPosition(_leftFoot, footArc);
+        SetFootPosition(_rightFoot, -footArc);
+    }
+
+    private void SetFootPosition(Transform foot, float footArc)
+    {
+        Vector3 position = foot.localPosition;
+        foot.localPosition = new Vector3(position.x, Mathf.Max(0, footArc) * _footArcHeight + _minY, position.z);
     }
 }
